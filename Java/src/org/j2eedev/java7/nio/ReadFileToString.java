a04973d87489e4bcd3d5316f14e956e7475279d6
Umashankar <umashankar@j2eedev.org>
2014-12-04 09:45:38 +0530

    Java 7 read file to string

--- Java/src/org/j2eedev/java7/nio/ReadFileToString.java
+++ Java/src/org/j2eedev/java7/nio/ReadFileToString.java
@@ -2,6 +2,10 @@ package org.j2eedev.java7.nio;
 import static java.lang.System.out;
 import static java.nio.file.Files.readAllBytes;
 import static java.nio.file.Paths.get;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 /**
  * 
  * @author Umashankar
@@ -16,6 +20,11 @@ public class ReadFileToString {
 		// TODO Auto-generated method stub
 		//readAllBytes -Files.readAllBytes  and get - Paths.get are static imports
 		out.println(new String(readAllBytes(get("websites.txt"))));
+		
+		Path path=Paths.get("websites.txt");
+		byte[] contents=Files.readAllBytes(path);
+		String fileContent =new String(contents);
+		out.println(fileContent);
 	}
 
 }
